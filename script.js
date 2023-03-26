d77301e87439a271f65b64f6deffff54c1cbb5c0
Saddam-tech <salokhiddinov0727@gmail.com>
2023-03-27 05:44:10 +0900

    latest changes

--- script.js
+++ script.js
@@ -74,28 +74,6 @@ const products = [
   },
 ];
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
-
 (async function () {
   const _products = [];
   for (let p of products) {
